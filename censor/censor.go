a54a3d66688b7bea57752c21348180512374d1fa
godcong <jumbycc@163.com>
2019-01-07 13:46:15 +0800

    add job detail

--- censor/censor.go
+++ censor/censor.go
@@ -4,11 +4,79 @@ import (
 	"github.com/godcong/aliyun-media-censor/util"
 	"net/http"
 	"net/url"
+	"time"
 )
 
-type ActionResponse struct {
+type JobResponse struct {
 	RequestID string `json:"RequestId"`
 	JobID     string `json:"JobId"`
+}
+
+type JobDetailResponse struct {
+	RequestID            string `json:"RequestId"`
+	MediaCensorJobDetail struct {
+		Input struct {
+			Bucket   string `json:"Bucket"`
+			Location string `json:"Location"`
+			Object   string `json:"Object"`
+		} `json:"Input"`
+		Suggestion       string `json:"Suggestion"`
+		DescCensorResult struct {
+			Scene      string `json:"Scene"`
+			Suggestion string `json:"Suggestion"`
+			Label      string `json:"Label"`
+			Rate       string `json:"Rate"`
+		} `json:"DescCensorResult"`
+		CreationTime      time.Time `json:"CreationTime"`
+		State             string    `json:"State"`
+		TitleCensorResult struct {
+			Scene      string `json:"Scene"`
+			Suggestion string `json:"Suggestion"`
+			Label      string `json:"Label"`
+			Rate       string `json:"Rate"`
+		} `json:"TitleCensorResult"`
+		PipelineID         string `json:"PipelineId"`
+		ID                 string `json:"Id"`
+		VensorCensorResult struct {
+			NextPageToken      string `json:"NextPageToken"`
+			CensorCensorResult struct {
+				CensorResult []struct {
+					Scene      string `json:"Scene"`
+					Suggestion string `json:"Suggestion"`
+					Label      string `json:"Label"`
+					Rate       string `json:"Rate"`
+				} `json:"CensorResult"`
+			} `json:"CensorCensorResult"`
+			VideoTimelines struct {
+				VideoTimeline []struct {
+					CensorCensorResult struct {
+						CensorResult []struct {
+							Scene      string `json:"Scene"`
+							Suggestion string `json:"Suggestion"`
+							Label      string `json:"Label"`
+							Rate       string `json:"Rate"`
+						} `json:"CensorResult"`
+					} `json:"CensorCensorResult"`
+					Timestamp string `json:"Timestamp"`
+					Object    string `json:"Object"`
+				} `json:"VideoTimeline"`
+			} `json:"VideoTimelines"`
+		} `json:"VensorCensorResult"`
+		BarrageCensorResult struct {
+			Scene      string `json:"Scene"`
+			Suggestion string `json:"Suggestion"`
+			Label      string `json:"Label"`
+			Rate       string `json:"Rate"`
+		} `json:"BarrageCensorResult"`
+		VideoCensorConfig struct {
+			VideoCensor bool `json:"VideoCensor"`
+			OutputFile  struct {
+				Bucket   string `json:"Bucket"`
+				Location string `json:"Location"`
+				Object   string `json:"Object"`
+			} `json:"OutputFile"`
+		} `json:"VideoCensorConfig"`
+	} `json:"MediaCensorJobDetail"`
 }
 
 const AliSite = "http://mts.cn-hangzhou.aliyuncs.com"
