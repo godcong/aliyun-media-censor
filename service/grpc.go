7e282d3c35acb2abe39c322f3efebc513e9ea93b
godcong <jumbycc@163.com>
2019-01-20 23:32:25 +0800

    update grpc

--- service/grpc.go
+++ service/grpc.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net"
 	"syscall"
-	"time"
 )
 
 // GRPCServer ...
@@ -20,20 +19,8 @@ type GRPCServer struct {
 	server *grpc.Server
 }
 
-// RemoteDownload ...
-func (s *GRPCServer) RemoteDownload(ctx context.Context, p *proto.RemoteDownloadRequest) (*proto.NodeReply, error) {
-	log.Printf("Received: %v", p.String())
-	stream := NewStreamerWithConfig(Config(), p.ObjectKey)
-	//stream.Dir, stream.FileName = filepath.Split(key)
-	stream.ObjectKey = p.ObjectKey
-	stream.SetEncrypt(false)
-	stream.Callback = Config().Callback.Type
-	//stream.SetURI("")
-	//stream.FileDest = config.Media.Upload
-	//stream.SetSrc(config.Media.Transfer)
-	globalQueue.Set(stream.ID, StatusQueuing, 0)
-	Push(stream)
-	return Result(nil), nil
+func (s *GRPCServer) Validate(context.Context, *proto.ValidateRequest) (*proto.CensorReply, error) {
+	panic("implement me")
 }
 
 type grpcBack struct {
@@ -41,54 +28,9 @@ type grpcBack struct {
 	BackAddr string
 }
 
-// Callback ...
-func (b *grpcBack) Callback(r *QueueResult) error {
-	log.Println("callback:", r.ID)
-	var conn *grpc.ClientConn
-	var err error
-
-	if b.BackType == "unix" {
-		conn, err = grpc.Dial("passthrough:///unix://"+b.BackAddr, grpc.WithInsecure())
-	} else {
-		conn, err = grpc.Dial(b.BackAddr, grpc.WithInsecure())
-	}
-
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	client := proto.NewManagerServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	reply, err := client.Back(ctx, &proto.ManagerCallbackRequest{
-		ID:     r.ID,
-		Detail: r.JSON(),
-	})
-
-	if err != nil {
-		return err
-	}
-	log.Printf("%+v", reply)
-	return nil
-}
-
-// NewGRPCBack ...
-func NewGRPCBack(cfg *Configure) StreamerCallback {
-	return &grpcBack{
-		BackType: DefaultString(cfg.Callback.BackType, "tcp"),
-		BackAddr: DefaultString(cfg.Callback.BackAddr, "localhost:7783"),
-	}
-}
-
-// Status ...
-func (s *GRPCServer) Status(ctx context.Context, p *proto.StatusRequest) (*proto.NodeReply, error) {
-	log.Printf("Received: %v", p.String())
-	return Result(nil), nil
-}
-
 // Result ...
-func Result(detail *proto.NodeReplyDetail) *proto.NodeReply {
-	return &proto.NodeReply{
+func Result(detail *proto.CensorReplyDetail) *proto.CensorReply {
+	return &proto.CensorReply{
 		Code:    0,
 		Message: "success",
 		Detail:  detail,
